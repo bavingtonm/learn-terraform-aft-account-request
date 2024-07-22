a22563a2df5094f2a53145058f090c4c17b2da63
Mark <bavingtonm@yahoo.co.uk>
2024-07-22 15:38:15 +0100

    Add sandbox1 account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "sandbox@cloudnine-solutions.co.uk"
-    AccountName               = "sandbox-aft"
+    AccountEmail              = "sandbox1@cloudnine-solutions.co.uk"
+    AccountName               = "sandbox1-aft"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "sandbox@cloudnine-solutions.co.uk"
-    SSOUserFirstName          = "sandbox"
+    SSOUserEmail              = "sandbox1@cloudnine-solutions.co.uk"
+    SSOUserFirstName          = "sandbox1"
     SSOUserLastName           = "AFT"
   }
 
@@ -23,5 +23,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "sandbox1"
 }
